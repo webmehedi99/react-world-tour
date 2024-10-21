172d93e0b58b3f29206157636532048fc13b0727
webmehedi99 <webmehedi99@gmail.com>
2024-10-21 09:06:03 +0600

    adding button to country and also add button color

--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,11 +1,16 @@
 import './Country.css'
+
 const Country = ({country}) => {
-    const {name, flags} = country ;
+    const {name, flags, population, area, cca3} = country ;
     console.log(country);
     return (
-        <div className='country'>
-            <h3>Name : {name?.common}</h3>
+        <div className='country '>
+            <h3>Name : {name.common}</h3>
             <img src={flags.png} alt="" />
+            <p>Population : {population}</p>
+            <p>Area : {area}</p>
+            <p><small>Code : {cca3}</small></p>
+            <button>Visited</button>
         </div>
     );
 };
